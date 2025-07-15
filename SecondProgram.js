e4ca374bbf103cea0bae472d505c73232cc794a7
RAMDAS009 <ramdasworkofficial@gmail.com>
2025-07-15 17:01:18 +0530

    Add files via upload
    Node js assignments

---
+++ SecondProgram.js
@@ -0,0 +1,7 @@
+let first = "this is first sentence";
+let second = "this is second sentence";
+let third = "this is third sentence";
+
+console.log("Characters in first sentence:", first.length);
+console.log("Characters in second sentence:", second.length);
+console.log("Characters in third sentence:", third.length);
